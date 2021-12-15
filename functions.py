c2b22ca25838dbda4f5d0995346669e9d414fa77
ituitis-canb18 <canbatuhan.official@gmail.com>
2021-12-15 17:53:39 +0300

    distance measurements init

--- functions.py
+++ functions.py
@@ -1,5 +1,10 @@
+from ctypes import WinDLL
 import numpy as np
 import pyzbar.pyzbar as pyzbar
+from scipy.spatial import distance as dist
+from imutils import perspective
+from imutils import contours
+
 import os
 import datetime
 import time
@@ -21,11 +26,24 @@ def detect_qr_and_print(frame, gray, cascade=QR_CASCADE):
             top = decoded_qr.rect.top
             left = decoded_qr.rect.left
 
-            cv.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 0), 2)
-            cv.rectangle(frame, (x-PADDING, y-PADDING), (x+w+PADDING, y+h+PADDING), (127, 0, 255), 2)
+            width = decoded_qr.rect.width
+            height = decoded_qr.rect.height
+
+            cv.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 0), 2) # raw detection
+            cv.rectangle(frame, (x-PADDING, y-PADDING), (x+w+PADDING, y+h+PADDING), (127, 0, 255), 2) # padded region of interest
+            cv.rectangle(frame, (x+left-PADDING, y+top-PADDING), (x+left+width-PADDING, y+top+height-PADDING), (213, 231, 76), 2) # detected qr
 
             cv.putText(frame, text_data, (x-PADDING+left, y-PADDING+top), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
             print(f"[x] {datetime.datetime.now().timestamp()} - {text_data}")
+
+            qr_location = {
+                "top_left":     (x+left-PADDING, y+top-PADDING),
+                "top_right":    (x+left+width-PADDING, y+top-PADDING),
+                "bottom_left":  (x+left-PADDING, y+top+height-PADDING),
+                "bottom_right": (x+left+width-PADDING, y+top+height-PADDING)
+            }
+
+            return qr_location
 
         except: pass
 
@@ -36,3 +54,19 @@ def iterate_and_detect_qrs(dir_path=BG_DIR_PATH):
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         detect_qr_and_print(img, gray)
         time.sleep(1)
+
+
+def midpoint(pointA, pointB):
+    return (
+        (pointA[0] + pointB[0]) * 0.5,
+        (pointA[1] + pointB[1]) * 0.5
+    )
+
+def calculate_distance(qr_location):
+    top_left_bot_left = midpoint(qr_location["top_left"], qr_location["bottom_left"])
+    top_right_bot_right = midpoint(qr_location["top_right"], qr_location["bottom_right"])
+    D = dist.euclidean(top_left_bot_left, top_right_bot_right)
+    ref_obj = (
+        (qr_location["top_left"], qr_location["top_right"], qr_location["bottom_right"], qr_location["bottom_left"]),
+        ####
+    )
--- settings.py
+++ settings.py
@@ -1,6 +1,8 @@
 import cv2 as cv
 
 QR_CASCADE = cv.CascadeClassifier("cascades/qr_cascade.xml")
-PADDING = 100
+PADDING = 50
 
 BG_DIR_PATH = "qrs_with_background"
+
+QR_WIDTH = 15*0.393700787 # cm to inches
